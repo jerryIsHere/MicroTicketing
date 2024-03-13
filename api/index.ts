b479d2ff3809be345182e760c41053d96fda3f49
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-13 15:52:09 +0800

    try apis

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,39 @@
 import express, { Express, Request, Response, Router } from "express";
 import google from "googleapis"
+import admin from 'firebase-admin';
 
 const api: Express = express();
 const port = process.env.PORT || 3000;
 
-var crediential = {
-  "web": {
-    "client_id": process.env.client_id,
-    "project_id": process.env.project_id,
-    "auth_uri": process.env.auth_uri,
-    "token_uri": process.env.token_uri,
-    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-    "client_secret": process.env.client_secret
+var googleapis_crediential = {
+    clientId: process.env.googleapis_web_client_id,
+    clientSecret: process.env.googleapis_web_client_secret
   }
-}
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: process.env.firebase_private_key,
+    clientEmail: process.env.firebase_client_email,
+    projectId: process.env.firebase_project_id
+  })
+});
 
 
 api.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  var db = admin.firestore();
+  var oauth2Client = new google.Auth.OAuth2Client({...googleapis_crediential, redirectUri: ""})
+  const scopes = [
+    'https://www.googleapis.com/auth/blogger',
+    'https://www.googleapis.com/auth/calendar'
+  ];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes
+  });
+  res.send("Express + TypeScript Server\n firebase collections: " + db.listCollections() + "\n oauth url: " + url);
+
 });
 api.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  admin;
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
