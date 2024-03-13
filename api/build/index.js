90cdc6592c3800d31f6ce8807fdf09d47c2b36c5
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-13 15:58:33 +0800

    test build

--- api/build/index.js
+++ api/build/index.js
@@ -4,23 +4,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const googleapis_1 = __importDefault(require("googleapis"));
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const api = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-var crediential = {
-    "web": {
-        "client_id": process.env.client_id,
-        "project_id": process.env.project_id,
-        "auth_uri": process.env.auth_uri,
-        "token_uri": process.env.token_uri,
-        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-        "client_secret": process.env.client_secret
-    }
+var googleapis_crediential = {
+    clientId: process.env.googleapis_web_client_id,
+    clientSecret: process.env.googleapis_web_client_secret
 };
+firebase_admin_1.default.initializeApp({
+    credential: firebase_admin_1.default.credential.cert({
+        privateKey: process.env.firebase_private_key,
+        clientEmail: process.env.firebase_client_email,
+        projectId: process.env.firebase_project_id
+    })
+});
 api.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+    var db = firebase_admin_1.default.firestore();
+    var oauth2Client = new googleapis_1.default.Auth.OAuth2Client(Object.assign(Object.assign({}, googleapis_crediential), { redirectUri: "" }));
+    const scopes = [
+        'https://www.googleapis.com/auth/blogger',
+        'https://www.googleapis.com/auth/calendar'
+    ];
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes
+    });
+    res.send("firebase collections: " + db.listCollections() + "\n oauth url: " + url);
 });
 api.listen(port, () => {
+    firebase_admin_1.default;
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
-export default api;
