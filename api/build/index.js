8ccd519842172a7d7e8cf44e8e663dcd60e6c37a
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-13 16:35:10 +0800

    new tsconfig.json

--- api/build/index.js
+++ api/build/index.js
@@ -1,27 +1,22 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const googleapis_1 = __importDefault(require("googleapis"));
-const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const api = (0, express_1.default)();
+import express from "express";
+import { google } from "googleapis";
+import admin from 'firebase-admin';
+const api = express();
 const port = process.env.PORT || 3000;
 var googleapis_crediential = {
     clientId: process.env.googleapis_web_client_id,
     clientSecret: process.env.googleapis_web_client_secret
 };
-firebase_admin_1.default.initializeApp({
-    credential: firebase_admin_1.default.credential.cert({
+admin.initializeApp({
+    credential: admin.credential.cert({
         privateKey: process.env.firebase_private_key,
         clientEmail: process.env.firebase_client_email,
         projectId: process.env.firebase_project_id
     })
 });
 api.get("/", (req, res) => {
-    var db = firebase_admin_1.default.firestore();
-    var oauth2Client = new googleapis_1.default.Auth.OAuth2Client(Object.assign(Object.assign({}, googleapis_crediential), { redirectUri: "" }));
+    var db = admin.firestore();
+    var oauth2Client = new google.auth.OAuth2(Object.assign(Object.assign({}, googleapis_crediential), { redirectUri: "" }));
     const scopes = [
         'https://www.googleapis.com/auth/blogger',
         'https://www.googleapis.com/auth/calendar'
@@ -33,6 +28,6 @@ api.get("/", (req, res) => {
     res.send("firebase collections: " + db.listCollections() + "\n oauth url: " + url);
 });
 api.listen(port, () => {
-    firebase_admin_1.default;
+    admin;
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
