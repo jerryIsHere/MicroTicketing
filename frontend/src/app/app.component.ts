321700686e96214de8dc01134f9526eb8d52c3ab
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-14 15:34:32 +0800

    google picker api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { GooglePickerService } from './service/google-picker.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(public googlePicker: GooglePickerService) {
+  }
+  selectFile() {
+    this.googlePicker.openPicker((result: google.picker.ResponseObject) => { })
+  }
 }
---
+++ frontend/src/app/service/google-picker.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class GooglePickerService {
+  public SCOPES: string = 'https://www.googleapis.com/auth/drive.file';
+
+  // TODO(developer): Set to client ID and API key from the Developer Console
+  private readonly CLIENT_ID = "242594859844-i56aphq64n5njgo09gt7e4j86530rlhu.apps.googleusercontent.com";
+  private readonly API_KEY = 'AIzaSyBmrhY1V7pPhsWqEIHLUZxE84gDUwzPJ1s';
+
+  // TODO(developer): Replace with your own project number from console.developers.google.com.
+  private readonly APP_ID: string = "242594859844";
+  //private picker;
+  private apiScriptElement: HTMLScriptElement = document.createElement('script');
+  private authScriptElement: HTMLScriptElement = document.createElement('script');
+
+  openPicker(pickerCallback: (result: google.picker.ResponseObject) => void) {
+    let tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: this.CLIENT_ID,
+      scope: this.SCOPES,
+      callback: (response) => {
+        if (response.error !== undefined) {
+          throw (response);
+        }
+        const view = new google.picker.DocsView(google.picker.ViewId.DOCS);
+        view.setMimeTypes('application/vnd.google-apps.folder,application/vnd.google-apps.spreadsheet');
+        const picker = new google.picker.PickerBuilder()
+          .enableFeature(google.picker.Feature.NAV_HIDDEN)
+          .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+          .setDeveloperKey(this.API_KEY)
+          .setAppId(this.APP_ID)
+          .setOAuthToken(response.access_token)
+          .addView(view)
+          .addView(new google.picker.DocsUploadView())
+          .setCallback(pickerCallback)
+          .build();
+        picker.setVisible(true);
+      }
+    });
+    tokenClient.requestAccessToken({ prompt: 'consent' });
+  }
+  loadScripts() {
+    let apiScriptPromise = new Promise((resolve, reject) => {
+      this.apiScriptElement.type = 'text/javascript';
+      this.apiScriptElement.src = "https://apis.google.com/js/api.js";
+      this.apiScriptElement.onload = () => {
+        gapi.load('client:picker', async () => {
+          await gapi.client.load('https://www.googleapis.com/discovery/v1/apis/drive/v3/rest');
+          resolve(null)
+        });
+        resolve(null)
+      }
+      document.getElementsByTagName('head')[0].appendChild(this.apiScriptElement);
+    });
+    let authScriptPromise = new Promise((resolve, reject) => {
+      this.authScriptElement.type = 'text/javascript';
+      this.authScriptElement.src = "https://apis.google.com/js/api.js";
+      this.authScriptElement.onload = () => {
+        resolve(null)
+      }
+      document.getElementsByTagName('head')[0].appendChild(this.authScriptElement);
+    });
+    return Promise.all([apiScriptPromise, authScriptPromise])
+  }
+  constructor() {
+    this.loadScripts().then(() => { });
+  }
+}
