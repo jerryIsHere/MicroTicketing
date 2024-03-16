98406c2dd4bc48e299679bbb5b27d32c3a8d87bd
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-16 19:10:52 +0800

    endpoint set

--- frontend/src/app/service/api.service.ts
+++ frontend/src/app/service/api.service.ts
@@ -5,8 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  //readonly endpoint: string = "http://localhost:3000"
-  readonly endpoint: string = "https://micro-ticketing-api.vercel.app"
+  readonly endpoint: string = location.origin.includes("localhost") ? "http://localhost:3000" : "https://micro-ticketing-api.vercel.app"
   constructor(public httpClient: HttpClient) { }
   getShowList(): Promise<object | undefined> {
     return this.httpClient.get(this.endpoint + "/shows").toPromise();
