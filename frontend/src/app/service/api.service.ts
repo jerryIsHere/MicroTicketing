30d5bb90789599fc9142884bc69698caa0b07be2
jerryIsHere <50871412+jerryIsHere@users.noreply.github.com>
2024-03-16 11:47:53 +0800

    slash

--- frontend/src/app/service/api.service.ts
+++ frontend/src/app/service/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  readonly endpoint: string = "https://micro-ticketing-api.vercel.app/"
+  readonly endpoint: string = "https://micro-ticketing-api.vercel.app"
   constructor(public httpClient: HttpClient) { }
   getShowList(): Promise<object | undefined> {
     return this.httpClient.get(this.endpoint + "/shows").toPromise();
